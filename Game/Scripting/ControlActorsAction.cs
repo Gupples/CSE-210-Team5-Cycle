7540b98c967b7fee6bb03ff51f3dbbbe32ba11c2
Thomas Moore <goldenpixels9@gmail.com>
2022-06-23 19:36:18 -0600

    Added second cycle along with JILK arrow keys

--- Game/Scripting/ControlActorsAction.cs
+++ Game/Scripting/ControlActorsAction.cs
@@ -14,6 +14,7 @@ namespace Unit05.Game.Scripting
     {
         private KeyboardService keyboardService;
         private Point direction = new Point(Constants.CELL_SIZE, 0);
+         private Point direction_two = new Point(Constants.CELL_SIZE, 0);
 
         /// <summary>
         /// Constructs a new instance of ControlActorsAction using the given KeyboardService.
@@ -26,34 +27,54 @@ namespace Unit05.Game.Scripting
         /// <inheritdoc/>
         public void Execute(Cast cast, Script script)
         {
-            // left
-            if (keyboardService.IsKeyDown("a") || keyboardService.IsKeyDown("left") || keyboardService.IsKeyDown("j"))
+            // left ----------------------------------------------------------------------------
+            if (keyboardService.IsKeyDown("a") || keyboardService.IsKeyDown("left"))
             {
                 direction = new Point(-Constants.CELL_SIZE, 0);
             }
 
-            // right
-            if (keyboardService.IsKeyDown("d") || keyboardService.IsKeyDown("right") || keyboardService.IsKeyDown("l"))
+            if (keyboardService.IsKeyDown("j"))
+            {
+                direction_two = new Point(-Constants.CELL_SIZE, 0);
+            }
+
+            // right ----------------------------------------------------------------------------
+            if (keyboardService.IsKeyDown("d") || keyboardService.IsKeyDown("right"))
             {
                 direction = new Point(Constants.CELL_SIZE, 0);
             }
 
-            // up
-            if (keyboardService.IsKeyDown("w")|| keyboardService.IsKeyDown("up") || keyboardService.IsKeyDown("i"))
+            if (keyboardService.IsKeyDown("l"))
+            {
+                direction_two = new Point(Constants.CELL_SIZE, 0);
+            }
+
+            // up ----------------------------------------------------------------------------
+            if (keyboardService.IsKeyDown("w")|| keyboardService.IsKeyDown("up"))
             {
                 direction = new Point(0, -Constants.CELL_SIZE);
             }
 
-            // down
-            if (keyboardService.IsKeyDown("s")|| keyboardService.IsKeyDown("down") || keyboardService.IsKeyDown("j"))
+            if (keyboardService.IsKeyDown("i"))
+            {
+                direction_two = new Point(0, -Constants.CELL_SIZE);
+            }
+
+            // down ----------------------------------------------------------------------------
+            if (keyboardService.IsKeyDown("s")|| keyboardService.IsKeyDown("down"))
             {
                 direction = new Point(0, Constants.CELL_SIZE);
+            }
+
+            if (keyboardService.IsKeyDown("k"))
+            {
+                direction_two = new Point(0, Constants.CELL_SIZE);
             }
 
             Cycle cycle = (Cycle)cast.GetFirstActor("cycle");
             Cycle cycle_two = (Cycle)cast.GetFirstActor("cycle_two");
             cycle.TurnHead(direction);
-            cycle_two.TurnHead(direction);
+            cycle_two.TurnHead(direction_two);
 
         }
     }
